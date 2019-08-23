5c030ff3977c64baf6b84dc33c33948fcb5be4d8
anshul <anshulagrawal455@gmail.com>
2019-08-23 15:15:33 +0530

    accepting token from headers too -for curl requests

--- src/middlewares/check-auth.js
+++ src/middlewares/check-auth.js
@@ -3,8 +3,9 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
     try {
-        const {token} = (req.method === "GET") ? req.query : req.body;
-        console.log(jwt.verify(token, process.env.JWT_SECRET))
+        let token = req.headers['x-access-token'] || req.headers['authorization']; // Express headers are auto converted to lowercase
+        const {token} = req.headers['x-access-token'] || req.headers['authorization'] || ( (req.method === "GET") ? req.query : req.body );
+       
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         
         req.userData = decoded;
