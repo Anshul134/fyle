ae85e8541c53a3e9a5cffc971fb890db348e4f6a
Anshul134 <anshulagrawal134@gmail.com>
2019-08-23 15:37:05 +0530

    Update check-auth.js

--- src/middlewares/check-auth.js
+++ src/middlewares/check-auth.js
@@ -5,8 +5,8 @@ module.exports = (req, res, next) => {
     try {
          console.log(req.headers);
         //let token = req.headers['x-access-token'] || req.headers['authorization']; // Express headers are auto converted to lowercase
-        const {token} = req.headers['x-access-token'] || req.headers['authorization'] || ( (req.method === "GET") ? req.query : req.body );
-       console.log("\n\n\ntoken::::::::::::::", token)
+        const {token} = req.headers['x-access-token'] || req.headers.authorization || ( (req.method === "GET") ? req.query : req.body );
+       console.log("\n\n\ntoken::::::::::::::", {token})
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         
         req.userData = decoded;
