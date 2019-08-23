6c0f6401a74254eefa0a269b8d04a02a242f0571
anshul <anshulagrawal455@gmail.com>
2019-08-23 11:42:45 +0530

    corrected limit and offset query

--- src/db/utils/sql.js
+++ src/db/utils/sql.js
@@ -1,4 +1,4 @@
 module.exports = {
-    FETCH_ONE : 'SELECT *, banks.name FROM branches INNER JOIN banks ON bank_id = id WHERE ifsc = $1 LIMIT = $2 OFFSET = $3',
-    FETCH_ALL : 'SELECT *,banks.name FROM branches LEFT JOIN banks ON id = bank_id WHERE name = $1 AND city = $2 LIMIT = $3 OFFSET = $4s'
+    FETCH_ONE : 'SELECT *, banks.name FROM branches INNER JOIN banks ON bank_id = id WHERE ifsc = $1 LIMIT $2 OFFSET $3',
+    FETCH_ALL : 'SELECT *,banks.name FROM branches LEFT JOIN banks ON id = bank_id WHERE name = $1 AND city = $2 LIMIT $3 OFFSET $4s'
 }
