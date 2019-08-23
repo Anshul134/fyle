c2d173512f2e55676ada02b54ff91babb518274b
anshul <anshulagrawal455@gmail.com>
2019-08-23 10:48:10 +0530

    sql query change

--- src/db/utils/sql.js
+++ src/db/utils/sql.js
@@ -1,4 +1,4 @@
 module.exports = {
-    FETCH_ONE : 'SELECT *, banks.name FROM branches INNER JOIN banks ON bank_id = id WHERE ifsc = $2',
+    FETCH_ONE : 'SELECT *, banks.name FROM branches INNER JOIN banks ON bank_id = id WHERE ifsc = $1',
     FETCH_ALL : 'SELECT *,banks.name FROM branches LEFT JOIN banks ON id = bank_id WHERE name = $1 AND city = $2'
 }
